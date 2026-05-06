79eea11edc0bd1e82bccad270e248b088e4d620b
shivangpandey5616-boop <shivangpandey5616@gmail.com>
2026-05-06 13:41:26 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const shopifyDomain = "entryleveljobs.onlinejob247.com";
+  const shopifyDomain = "bebee.com";
   const proxyHost = req.headers.host;
 
   const targetURL = `https://${shopifyDomain}${req.url}`;
